a3ba2c68e079f736850eb61e25dff47a249e4b54
Subhra Dey <sdbappi@Subhras-MBP.attlocal.net>
2023-06-30 16:23:34 -0400

    created new class Person

---
+++ src/main/java/com/ivoyant/test/Person.java
@@ -0,0 +1,18 @@
+package com.ivoyant.test;
+
+public class Person {
+private String name;
+    private String address;
+    private String phoneNumber;
+    private String email;
+
+    public Person() {
+    }
+
+    public Person(String name, String address, String phoneNumber, String email) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
